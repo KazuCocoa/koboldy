cb9e623efc959e7a6589f9aa458a046c12b163f0
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-07-06 23:58:07 +0900

    update kobold running scripts

--- lib/koboldy/run.rb
+++ lib/koboldy/run.rb
@@ -3,85 +3,137 @@ require "json"
 require_relative "io"
 
 class Koboldy
-  module Run
-    class << self
-      # @param [String] expected_folder A path to expected folder which have expected images.
-      # @param [String] test_target_folder A path to tested folder which have tested images.
-      # @param [String] compared_folder A path to highlighted folder.
-      # @param [String] configuration_file A path to configuration file.
-      # @param [String] base_uri A path to base uri which is as current directory.
-      def kobold_command(expected_folder, test_target_folder, compared_folder, configuration_file, base_uri)
-        "$(npm bin)/kobold --config #{configuration_file} --approved-folder #{expected_folder} " +
-          "--highlight-folder #{compared_folder} --build-folder #{test_target_folder} " +
-          "--fail-orphans --fail-additions #{base_uri}"
+  class Run
+    def initialize(kobold_cmd = "kobold")
+      @cmd = kobold_cmd
+    end
+
+    def clear_cmd(kobold_cmd = "kobold")
+      @cmd = kobold_cmd
+    end
+
+    # @param [String] file A path to configuration file
+    # @return [Self]
+    def config(file)
+      @cmd.concat(" --config #{file}")
+      self
+    end
+
+    # @param [String] expected_folder A path to expected files
+    # @return [Self]
+    def approved(expected_folder)
+      @cmd.concat(" --approved-folder #{expected_folder}")
+      self
+    end
+
+    # @param [String] compared_folder A path to compared files
+    # @return [Self]
+    def highlight(compared_folder)
+      @cmd.concat(" --highlight-folder #{compared_folder}")
+      self
+    end
+
+    # @param [String] test_target_folder A path to test target files
+    # @return [Self]
+    def build(test_target_folder)
+      @cmd.concat(" --build-folder #{test_target_folder}")
+      self
+    end
+
+    # Add "--fail-orphans" option
+    # @return [Self]
+    def fail_orphans
+      @cmd.concat(" --fail-orphans")
+      self
+    end
+
+    # Add "--fail-additions" option
+    # @return [Self]
+    def fail_additions
+      @cmd.concat(" --fail-additions")
+      self
+    end
+
+    # @param [String] command Add aditional options.
+    # @return [Self]
+    def additional_option(command)
+      @cmd.concat(" #{command}")
+      self
+    end
+
+    # @param [String] path Path of current path.
+    def base_uri(path)
+      @cmd.concat(" #{path}")
+    end
+
+    # @param [String] in_file_path A path to file which you would like to save standard output and error.
+    # @return [String] File path which is saved results.
+    def check_and_save(in_file_path = "./tmp/kobold.txt")
+      fail "no command: #{@cmd}" if @cmd.nil?
+      Koboldy::Io.capture(@cmd, in_file_path)
+      in_file_path
+    end
+
+    # @param [String] from_path A path you would like to analysis outputs by kobold command.
+    # @param [String] into_file(Option) A path you would like to save the result. If into_file is empty, then the method return JSON.
+    # @return [String] into_file A path to saved result file.
+    def results(from_path, into_file = "")
+      result = File.read(from_path)
+      add, orp, dif = additions(result), orphans(result), different(result)
+
+      hash = {
+        :passing => passing(result),
+        :failing =>  failing(result),
+        :pending => pending(result),
+        :additions => { count: add.length, messages: add }, # 期待結果に無い
+        :orphans => { count: orp.length, messages: orp }, # 比較対象に無い
+        :different => { count: dif.length, messages: dif }, # 比較結果が異なる
+      }
+
+      return JSON.pretty_generate(hash) if into_file.empty?
+
+      File.write(into_file, JSON.pretty_generate(hash))
+      into_file
+    end
+
+    private
+
+    def passing(result)
+      passing = result.scan(/.*passing.*/).first.to_s
+      find_score(passing)
+    end
+
+    def failing(result)
+      failing = result.scan(/.*failing.*/).first.to_s
+      find_score(failing)
+    end
+
+    def pending(result)
+      pending = result.scan(/.*pending.*/).first.to_s
+      find_score(pending)
+    end
+
+    def find_score(scored_line)
+      return 0 if scored_line.nil? || scored_line.empty?
+      scored_line.split(/\s/).find { |item| item =~ /\A\d+\z/ }.to_i
+    end
+
+    def additions(result)
+      result.scan(/Screen is new:.*/).map do |line|
+        line.to_s.split(/\u001b\[0m\u001b\[90m/)
       end
+    end
 
-      # @param [String] cmd A command you would like to run,
-      # @param [String] in_file_path A path to file which you would like to save standard output and error.
-      # @return [String] File path which is saved results.
-      def check_and_save(cmd, in_file_path = "./tmp/kobold.txt")
-        Koboldy::Io.capture(cmd, in_file_path)
-        in_file_path
+    def orphans(result)
+      result.scan(/Error: Approved screen is orphaned.*/).map do |line|
+        line.to_s.split(/\u001b\[0m\u001b\[90m/)
       end
+    end
 
-      # @param [String] from_path A path you would like to analysis outputs by kobold command.
-      # @param [String] into_file A path you would like to save the result.
-      # @return [String] into_file A path to saved result file.
-      def results(from_path, into_file)
-        result = File.read(from_path)
-        add, orp, dif = additions(result), orphans(result), different(result)
-
-        hash = {
-          :passing => passing(result),
-          :failing =>  failing(result),
-          :pending => pending(result),
-          :additions => { count: add.length, messages: add }, # 期待結果に無い
-          :orphans => { count: orp.length, messages: orp }, # 比較対象に無い
-          :different => { count: dif.length, messages: dif }, # 比較結果が異なる
-        }
-        File.write(into_file, JSON.pretty_generate(hash))
-        into_file
+    def different(result)
+      result.scan(/Error: Screens are different for.*/).map do |line|
+        line.to_s.split(/\u001b\[0m\u001b\[90m/)
       end
-
-      private
-
-      def passing(result)
-        passing = result.scan(/.*passing.*/).first.to_s
-        find_score(passing)
-      end
-
-      def failing(result)
-        failing = result.scan(/.*failing.*/).first.to_s
-        find_score(failing)
-      end
-
-      def pending(result)
-        pending = result.scan(/.*pending.*/).first.to_s
-        find_score(pending)
-      end
-
-      def find_score(scored_line)
-        return 0 if scored_line.nil? || scored_line.empty?
-        scored_line.split(/\s/).find { |item| item =~ /\A\d+\z/ }.to_i
-      end
-
-      def additions(result)
-        result.scan(/Screen is new:.*/).map do |line|
-          line.to_s.split(/\u001b\[0m\u001b\[90m/)
-        end
-      end
-
-      def orphans(result)
-        result.scan(/Error: Approved screen is orphaned.*/).map do |line|
-          line.to_s.split(/\u001b\[0m\u001b\[90m/)
-        end
-      end
-
-      def different(result)
-        result.scan(/Error: Screens are different for.*/).map do |line|
-          line.to_s.split(/\u001b\[0m\u001b\[90m/)
-        end
-      end
-    end # class << self
-  end # module Run
+    end
+  end # class Run
 end # module Kobold
---
+++ test/kobold/run_test.rb
@@ -0,0 +1,78 @@
+require "test/unit"
+
+require "./lib/koboldy/run"
+
+class RunTest < Test::Unit::TestCase
+
+  # Called before every test method runs. Can be used
+  # to set up fixture information.
+  def setup
+    @run = Koboldy::Run.new("kobold")
+  end
+
+  def teardown
+    # Do nothing
+  end
+
+
+  def test_cmd_configuration
+    expected = "kobold --config a --approved-folder b --highlight-folder c --build-folder d --fail-orphans --fail-additions neko"
+    cmd = @run
+            .config("a")
+            .approved("b")
+            .highlight("c")
+            .build("d")
+            .fail_orphans
+            .fail_additions
+            .base_uri("neko")
+    assert_equal(cmd, expected)
+  end
+
+  def test_result_check
+    expected = <<-EOS.chomp
+{
+  "passing": 250,
+  "failing": 101,
+  "pending": 0,
+  "additions": {
+    "count": 2,
+    "messages": [
+      [
+        "Screen is new: 0004_03_01_bargain_store_details"
+      ],
+      [
+        "Screen is new: 0017_01_05_open_contact5"
+      ]
+    ]
+  },
+  "orphans": {
+    "count": 3,
+    "messages": [
+      [
+        "Error: Approved screen is orphaned: 0000_01_03_hot_recommend"
+      ],
+      [
+        "Error: Approved screen is orphaned: 0102_02_01_open_recipe_2446163"
+      ],
+      [
+        "Error: Approved screen is orphaned: 0102_02_02_open_tukurepo_2446163"
+      ]
+    ]
+  },
+  "different": {
+    "count": 2,
+    "messages": [
+      [
+        "Error: Screens are different for 0000_01_04_recent"
+      ],
+      [
+        "Error: Screens are different for 0000_01_05_my_folder"
+      ]
+    ]
+  }
+}
+EOS
+    result = @run.results("./test/data/kobold_test_data.txt")
+    assert_equal(result, expected)
+  end
+end

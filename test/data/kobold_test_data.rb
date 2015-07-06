e95ab9af942bbdbd549b1ff1dfad5a686394a2c7
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-07-06 23:16:39 +0900

    add tests

---
+++ test/data/kobold_test_data.rb
@@ -0,0 +1,100 @@
+TEST_DATA_KOBOLD = <<-EOS
+[0m    9100 - 05 - 01 - mypage[0m
+    [32m  âœ“[0m[90m should load the approved screen [0m
+    [32m  âœ“[0m[90m should load the build screen [0m
+    [32m  âœ“[0m[90m should load the configuration of screen [0m
+    [31m  99) should be similar[0m
+[0m    9100 - 05 - 02 - myfolders[0m
+    [32m  âœ“[0m[90m should load the approved screen [0m
+    [32m  âœ“[0m[90m should load the build screen [0m
+    [32m  âœ“[0m[90m should load the configuration of screen [0m
+    [31m  100) should be similar[0m
+[0m    9102 - rotate[0m
+    [32m  âœ“[0m[90m should load the approved screen [0m
+    [32m  âœ“[0m[90m should load the build screen [0m
+    [32m  âœ“[0m[90m should load the configuration of screen [0m
+    [31m  101) should be similar[0m
+
+
+[92m [0m[32m 250 passing[0m[90m (1m)[0m
+[31m  101 failing[0m
+
+[0m  1) Kobold 0000 - 01 - 03 - hot - recommend is orphaned:
+[0m[31m     Error: Approved screen is orphaned: 0000_01_03_hot_recommend[0m[90m
+      at Context.<anonymous> (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/lib/kobold.js:224:13)
+      at callFn (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runnable.js:249:21)
+      at Test.Runnable.run (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runnable.js:242:7)
+      at Runner.runTest (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:373:10)
+      at /Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:451:12
+      at next (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:298:14)
+      at /Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:308:7
+      at next (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:246:23)
+      at Immediate._onImmediate (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:275:5)
+      at processImmediate [as _immediateCallback] (timers.js:358:17)
+[0m
+[0m  2) Kobold 0102 - 02 - 01 - open - recipe - 2446163 is orphaned:
+[0m[31m     Error: Approved screen is orphaned: 0102_02_01_open_recipe_2446163[0m[90m
+      at Context.<anonymous> (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/lib/kobold.js:224:13)
+      at callFn (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runnable.js:249:21)
+      at Test.Runnable.run (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runnable.js:242:7)
+      at Runner.runTest (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:373:10)
+      at /Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:451:12
+      at next (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:298:14)
+      at /Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:308:7
+      at next (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:246:23)
+      at Immediate._onImmediate (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:275:5)
+      at processImmediate [as _immediateCallback] (timers.js:358:17)
+[0m
+[0m  3) Kobold 0102 - 02 - 02 - open - tukurepo - 2446163 is orphaned:
+[0m[31m     Error: Approved screen is orphaned: 0102_02_02_open_tukurepo_2446163[0m[90m
+      at Context.<anonymous> (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/lib/kobold.js:224:13)
+      at callFn (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runnable.js:249:21)
+      at Test.Runnable.run (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runnable.js:242:7)
+      at Runner.runTest (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:373:10)
+      at /Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:451:12
+      at next (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:298:14)
+      at /Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:308:7
+      at next (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:246:23)
+      at Immediate._onImmediate (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:275:5)
+      at processImmediate [as _immediateCallback] (timers.js:358:17)
+[0m  11) Kobold 0004 - 03 - 01 - bargain - store - details is an addition:
+[0m[31m     Error: Screen is new: 0004_03_01_bargain_store_details[0m[90m
+      at Context.<anonymous> (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/lib/kobold.js:264:13)
+      at callFn (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runnable.js:249:21)
+      at Test.Runnable.run (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runnable.js:242:7)
+      at Runner.runTest (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:373:10)
+      at /Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:451:12
+      at next (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:298:14)
+      at /Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:308:7
+      at next (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:246:23)
+      at Immediate._onImmediate (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:275:5)
+      at processImmediate [as _immediateCallback] (timers.js:358:17)
+[0m
+[0m  12) Kobold 0017 - 01 - 05 - open - contact5 is an addition:
+[0m[31m     Error: Screen is new: 0017_01_05_open_contact5[0m[90m
+      at Context.<anonymous> (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/lib/kobold.js:264:13)
+      at callFn (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runnable.js:249:21)
+      at Test.Runnable.run (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runnable.js:242:7)
+      at Runner.runTest (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:373:10)
+      at /Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:451:12
+      at next (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:298:14)
+      at /Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:308:7
+      at next (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:246:23)
+      at Immediate._onImmediate (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/mocha/lib/runner.js:275:5)
+      at processImmediate [as _immediateCallback] (timers.js:358:17)
+[0m
+[0m  21) Kobold 0000 - 01 - 04 - recent should be similar:
+[0m[31m     Error: Screens are different for 0000_01_04_recent[0m[90m
+      at /Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/lib/kobold.js:453:12
+      at /Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/promise/lib/core.js:33:15
+      at flush (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/promise/node_modules/asap/asap.js:27:13)
+      at process._tickCallback (node.js:355:11)
+[0m
+[0m  22) Kobold 0000 - 01 - 05 - my - folder should be similar:
+[0m[31m     Error: Screens are different for 0000_01_05_my_folder[0m[90m
+      at /Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/lib/kobold.js:453:12
+      at /Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/promise/lib/core.js:33:15
+      at flush (/Users/kazuaki-matsuo/Documents/github/mobile_automated_testing_appium/node_modules/kobold/node_modules/promise/node_modules/asap/asap.js:27:13)
+      at process._tickCallback (node.js:355:11)
+[0m
+EOS
--- test/kobold/conf_test.rb
+++ test/kobold/conf_test.rb
@@ -1,23 +1,106 @@
-require "minitest/autorun"
+require "test/unit"
+require "rantly"
 
-class KoboldyConf < Minitest::Test
+require "./lib/koboldy/conf"
 
-  # Called before every test method runs. Can be used
-  # to set up fixture information.
+class TestKobold < Test::Unit::TestCase
   def setup
-    # Do nothing
+    @kobold ||= Koboldy::Conf.instance
   end
-
-  # Called after every test method runs. Can be used to tear
-  # down fixture information.
 
   def teardown
-    # Do nothing
+    @kobold.clear_config
   end
 
-  # Fake test
-  def test_fail
+  data(                      #  x       y     width    height
+    "test1" => Rantly { [integer, integer, integer, integer] })
+  test "#add_block_out" do |(x, y, width, height)|
+    expected = [{ x: x, y: y,width: width, height: height }]
+    @kobold.add_block_out(x, y, width, height)
+    assert_equal(@kobold.config["blockOut"], expected)
 
-    fail('Not implemented')
+    expected.push({ x: x, y: y,width: width, height: height })
+    @kobold.add_block_out(x, y, width, height)
+    assert_equal(@kobold.config["blockOut"], expected)
   end
+
+  data(          # on/off
+    "test1" => [true],
+    "test2" => [false])
+  test "#add_hide_shift" do |(boolean)|
+    @kobold.add_hide_shift boolean
+    assert_equal(@kobold.config["hideShift"], boolean)
+  end
+
+
+  data(          # on/off
+    "test1" => [true],
+    "test2" => [false])
+  test "#add_compose_left_to_right" do |(boolean)|
+    @kobold.add_compose_left_to_right boolean
+    assert_equal(@kobold.config["composeLeftToRight"], boolean)
+  end
+
+  data(       # expected  value
+    "test1" => [  0,     -1],
+    "test2" => [  0,      0],
+    "test3" => [255,    255],
+    "test4" => [255,    256])
+  test "#add_block_out_red" do |(expected, actual)|
+    @kobold.add_block_out_red actual
+    assert_equal(@kobold.config["blockOutRed"], expected)
+  end
+
+  data(       # expected  value
+    "test1" => [  0,     -1],
+    "test2" => [  0,      0],
+    "test3" => [255,    255],
+    "test4" => [255,    256])
+  test "#add_block_out_blue" do |(expected, actual)|
+    @kobold.add_block_out_blue actual
+    assert_equal(@kobold.config["blockOutBlue"], expected)
+  end
+
+  data(       # expected  value
+    "test1" => [  0,     -1],
+    "test2" => [  0,      0],
+    "test3" => [255,    255],
+    "test4" => [255,    256])
+  test "#add_block_out_green" do |(expected, actual)|
+    @kobold.add_block_out_green actual
+    assert_equal(@kobold.config["blockOutGreen"], expected)
+  end
+
+
+
+  test "add some items" do
+    @kobold.add_block_out(0, 0, 0, 0)
+      .add_block_out(50, 50, 50, 50)
+      .add_block_out_red(255)
+      .add_compose_left_to_right(true)
+
+    expected =<<-EOS.chomp
+{
+  "blockOut": [
+    {
+      "x": 0,
+      "y": 0,
+      "width": 0,
+      "height": 0
+    },
+    {
+      "x": 50,
+      "y": 50,
+      "width": 50,
+      "height": 50
+    }
+  ],
+  "blockOutRed": 255,
+  "composeLeftToRight": true
+}
+    EOS
+    assert_equal(JSON.pretty_generate(@kobold.config.to_h), expected)
+  end
+
+
 end
